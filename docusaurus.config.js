70132a8dd6bc020339c80a9fd329ca88b337ede2
Nuri Hodges <nuri@irunbackwards.com>
2022-02-18 09:31:27 -0800

    chore: update Docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,15 +6,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'Lost SamuRise - Documentation',
+  tagline: 'Lost SamuRise - Documentation',
+  url: 'https://docs.samurise.xyz',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'nhodges', // Usually your GitHub org/user name.
+  projectName: 'samurise', // Usually your repo name.
 
   presets: [
     [
